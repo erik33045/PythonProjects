be4e8a1732f9c34ff82164d7b1fc16288e0e5659
erik <erik33045@gmail.com>
2014-03-09 14:22:44 -0400

    Finished the code for the linearly complex dynamic algorithm.

--- AlgorithmsDynamicProgrammingProject.py
+++ AlgorithmsDynamicProgrammingProject.py
@@ -1,27 +1,135 @@
+class Cell:
+    def __init__(self):
+        pass
+
+    val = 0
+    parent = 0
 
 
-def process_case(input_file, output_file):
-    number_of_weeks = int(input_file.readline())
-    job_list = [input_file.readline().split(), input_file.readline().split()]
-
-    input_file.readline()
-
-    output_file.write(str(do_most_work(job_list, number_of_weeks - 1, True)) + '\n')
-
-
-def do_most_work(job_array, current_week, work_on_last_week):
+def do_most_work_recursive(job_array, current_week, work_on_last_week):
     low_value = int(job_array[0][current_week])
     high_value = int(job_array[1][current_week])
 
     if current_week == -1:
         return 0
     else:
-        max_previous_no_work = do_most_work(job_array, current_week - 1, False)
-        max_previous_work = do_most_work(job_array, current_week - 1, True)
+        max_previous_no_work = do_most_work_recursive(job_array, current_week - 1, False)
+        max_previous_work = do_most_work_recursive(job_array, current_week - 1, True)
         if work_on_last_week:
             return max(max_previous_work + low_value, max_previous_no_work + high_value)
         else:
-            return max_previous_work #because it will always be greater than max_previous_no_work
+            return max_previous_work
+
+
+def print_tables(value_table):
+    for i in range(len(value_table)):
+        print ''
+        for j in range(len(value_table[i])):
+            print str(value_table[i][j].val) + ' ',
+    print ''
+    for i in range(len(value_table)):
+        print ''
+        for j in range(len(value_table[i])):
+            print str(value_table[i][j].parent) + ' ',
+    print ' \n'
+
+
+# noinspection PyPep8Naming
+def do_most_work(job_array, number_of_weeks, output_file):
+    LOW = 0
+    HIGH = 1
+    NOTHING = 2
+    value_table = []
+
+    for i_iterator in range(number_of_weeks+1):
+        value_table.append([])
+        for j_iterator in range(3):
+            value_table[i_iterator].append(Cell())
+
+    if number_of_weeks == 0:
+        return 0
+
+    for y in range(LOW, NOTHING+1):
+        value_table[0][y].val = 0
+        value_table[0][y].parent = NOTHING
+        value_table[1][y].parent = NOTHING
+
+    for y in range(1, number_of_weeks+1):
+        value_table[y][HIGH].parent = NOTHING
+
+    for x in range(1, number_of_weeks+1):
+        l_val = int(job_array[LOW][x])
+        h_val = int(job_array[HIGH][x])
+        prev_l = value_table[x - 1][LOW].val
+        prev_h = value_table[x - 1][HIGH].val
+        prev_n = value_table[x - 1][NOTHING].val
+
+        value_table[x][HIGH].val = prev_n + h_val
+
+        if prev_h > prev_l:
+            value_table[x][LOW].val = prev_h + l_val
+            value_table[x][LOW].parent = HIGH
+
+            value_table[x][NOTHING].val = prev_h
+            value_table[x][NOTHING].parent = HIGH
+
+        else:
+            value_table[x][LOW].val = prev_l + l_val
+            value_table[x][LOW].parent = LOW
+            value_table[x][NOTHING].val = prev_l
+            value_table[x][NOTHING].parent = LOW
+
+    low_final = value_table[number_of_weeks][LOW].val
+    high_final = value_table[number_of_weeks][HIGH].val
+
+    #Debug info, allows us to see the traceback and parent tables
+    #print_tables(value_table)
+
+    if high_final > low_final:
+        output_file.write(str(high_final) + '\n')
+        output_file.write(reconstruct_path(value_table, HIGH, number_of_weeks) + '\n')
+    else:
+        output_file.write(str(low_final) + '\n')
+        output_file.write(reconstruct_path(value_table, LOW, number_of_weeks) + '\n')
+
+
+# noinspection PyPep8Naming
+def reconstruct_path(value_table, step, week):
+    LOW = 0
+    HIGH = 1
+    NOTHING = 2
+
+    return_string = ""
+
+    if step == LOW:
+        if week != 1:
+            return_string += reconstruct_path(value_table, value_table[week][LOW].parent, week - 1)
+        return_string += 'L' + ' '
+    elif step == HIGH:
+        if week != 1:
+            return_string += reconstruct_path(value_table, value_table[week][HIGH].parent, week - 1)
+        return_string += 'H' + ' '
+    elif step == NOTHING:
+        if week != 1:
+            return_string += reconstruct_path(value_table, value_table[week][NOTHING].parent, week - 1)
+        return_string += 'N' + ' '
+    return return_string
+
+
+def process_case(input_file, output_file):
+    number_of_weeks = int(input_file.readline())
+    job_list = [[0], [0]]
+
+    low_value_list = input_file.readline().split()
+    high_value_list = input_file.readline().split()
+
+    [job_list[0].append(int(value)) for value in low_value_list]
+    [job_list[1].append(int(value)) for value in high_value_list]
+
+    input_file.readline()
+
+    do_most_work(job_list, number_of_weeks, output_file)
+    output_file.write('\n')
 
 
 def dynamic_programming():
