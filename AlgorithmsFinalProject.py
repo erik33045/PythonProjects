aaab029286f023a1e1817f4da57b9f46ceddc1b2
erik <erik33045@gmail.com>
2014-04-17 11:16:14 -0400

    -Added a graph input file generator, and added a few tests -Added adams solution to the problem in a function inside AlgorithmsFinalProject.py

--- AlgorithmsFinalProject.py
+++ AlgorithmsFinalProject.py
@@ -31,5 +31,70 @@ def algorithms_final_project():
     plt.savefig("output", dpi=90)
 
 
+def graph_generator(number_of_nodes, connectedness_of_graph, name_of_test_file):
+    connectedness_ratio = float(connectedness_of_graph / float(100))
+    number_of_edges_per_node = int(number_of_nodes * connectedness_ratio)
+
+    output_file = open(name_of_test_file, 'wb')
+
+    total_number_of_edges = 0
+    for node in range(1, number_of_nodes+1):
+        for edge in range(1, number_of_edges_per_node+1):
+            if edge == node:
+                continue
+            total_number_of_edges += 1
+
+    output_file.write(str(number_of_nodes) + " " + str(total_number_of_edges) + "\n")
+
+    for node in range(1, number_of_nodes+1):
+        for edge in range(1, number_of_edges_per_node+1):
+            if edge == node:
+                continue
+            output_file.write(str(node) + " " + str(edge) + "\n")
+
+
+def adams_solution():
+    input_file = open('input.txt', 'r')
+
+    g = nx.Graph()
+    vals = input_file.readline().split()
+
+    count = 0
+    max_count = 0
+    ordered = []
+    check = []
+
+    for i in range(1, int(vals[1])+1):
+        edge = input_file.readline().split()
+        edge = map(int, edge)
+        u = edge[0]
+        v = edge[1]
+        g.add_edge(u, v)
+
+    for i in range(1, len(g.nodes())+1):
+        ordered.append([i, sorted(g[i].keys())])
+
+    ordered.sort(key=lambda s: len(s[1]), reverse=True)
+
+    node_num = len(g.nodes()) / 2
+
+    for i in range(0, node_num):
+        check.append(ordered[i][0])
+        c = check[-1]
+        length = len(g.neighbors(c)) - (len(set(g.neighbors(c)).intersection(check))) * 2
+        count += length
+        if count > max_count:
+            max_count = count
+        elif count < max_count:
+            del check[-1]
+
+    print '\n'
+    print 'max crossing edges:', max_count
+    difference = []
+    input_file.close()
+
+
 if __name__ == "__main__":
     algorithms_final_project()
+
+
--- BasePage.py
+++ BasePage.py
@@ -4,4 +4,9 @@ import AlgorithmsFinalProject
 
 
 if '__main__' == __name__:
+    number_of_nodes = 100
+    connectedness_of_graph = 1
+    name_of_test_file = "input-test.txt"
+
+    AlgorithmsFinalProject.graph_generator(number_of_nodes, connectedness_of_graph, name_of_test_file)
     AlgorithmsFinalProject.algorithms_final_project()
