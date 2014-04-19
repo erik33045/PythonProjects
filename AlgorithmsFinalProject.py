0ffec74ba26a85e422899a3c481da81853138ef7
vtran21 <viktran@mail.usf.edu>
2014-04-18 23:02:13 -0400

    Edits to Adam's Solution: Time elapsed in milliseconds added print nodes for each group added

--- AlgorithmsFinalProject.py
+++ AlgorithmsFinalProject.py
@@ -1,6 +1,7 @@
 import networkx as nx
 import random
 #import matplotlib.pyplot as plt
+import datetime
 
 
 #randomization is a good factor to emply
@@ -54,47 +55,65 @@ def graph_generator(number_of_nodes, connectedness_of_graph, name_of_test_file):
                 continue
             output_file.write(str(node) + " " + str(edge) + "\n")
 
-def adams_solution(input_file):
-  g = nx.Graph() # create the graph for edge/node storage
 
-  vals = input_file.readline().split() # grab the first two values nodes/edges
+def adams_solution(input_filename):
+    start_time = datetime.datetime.now()
+    g = nx.Graph() # create the graph for edge/node storage
+    input_file = open(input_filename, "r")
+    vals = input_file.readline().split() # grab the first two values nodes/edges
 
-  count = 0 # check the count of the current grouping
-  max_count = 0 # set the cap for the max count encountered
-  neighbors = [] # a list of the nodes with their number of neighbors
-  second_group = [] # a second list used to form a second_group
+    count = 0 # check the count of the current grouping
+    max_count = 0 # set the cap for the max count encountered
+    neighbors = [] # a list of the nodes with their number of neighbors
+    first_group = []
+    second_group = [] # a second list used to form a second_group
 
-  # read in each line and add the corresponding edge to the graph, g
-  for i in range(1, int(vals[1])+1):
-    edge = input_file.readline().split()
-    edge = map(int, edge)
-    u = edge[0]
-    v = edge[1]
-    g.add_edge(u, v)
 
-  # place all the nodes and their neighbors into the neighbors list
-  for i in range(1, len(g.nodes())+1):
-    neighbors.append([i,g[i].keys()])
+    # read in each line and add the corresponding edge to the graph, g
+    for i in range(1, int(vals[1])+1):
+        edge = input_file.readline().split()
+        edge = map(int, edge)
+        u = edge[0]
+        v = edge[1]
+        g.add_edge(u, v)
 
-  # the number of nodes to check (default to all of them)
-  node_num = len(g.nodes())
+    # place all the nodes and their neighbors into the neighbors list
+    for i in range(1, len(g.nodes())+1):
+        neighbors.append([i,g[i].keys()])
 
-  # check nodes until you reach the cap of num_nodes
-  for i in range(0, node_num):
-    second_group.append(neighbors[i][0]) # place the next node into the second group
-    last_node = second_group[-1] # create a reference to the last node put into the second group
-    # set crossing_edges to the number of neighbors the most recently added node has
-    # and remove any connections to nodes already in the group
-    crossing_edges = len(g.neighbors(last_node)) - (len(set(g.neighbors(last_node)).intersection(second_group))) * 2
-    count += crossing_edges # append these crossing_edges to the count of crossing edges
-    if count > max_count: # if the current configuration is better than the previous best...
-      max_count = count # set this new max value
-    elif count < max_count: # if this new configuration isn't better than the best...
-      del second_group[-1] # remove the most recently added node and keep going
+    # the number of nodes to check (default to all of them)
+    node_num = len(g.nodes())
 
-  print 'max crossing edges:', max_count
+    # check nodes until you reach the cap of num_nodes
+    for i in range(0, node_num):
+        second_group.append(neighbors[i][0]) # place the next node into the second group
+        last_node = second_group[-1] # create a reference to the last node put into the second group
+        # set crossing_edges to the number of neighbors the most recently added node has
+        # and remove any connections to nodes already in the group
+        crossing_edges = len(g.neighbors(last_node)) - (len(set(g.neighbors(last_node)).intersection(second_group))) * 2
+        count += crossing_edges # append these crossing_edges to the count of crossing edges
+        if count > max_count: # if the current configuration is better than the previous best...
+            max_count = count # set this new max value
+        elif count < max_count: # if this new configuration isn't better than the best...
+            del second_group[-1] # remove the most recently added node and keep going
+            first_group.append(neighbors[i][0])#append to first_group
 
-  input_file.close() # close the file
+    elapsed_time = datetime.datetime.now() - start_time
+
+    print elapsed_time.microseconds*10^3
+    print max_count
+
+    for n in first_group:
+        print n,
+
+    print
+
+    for n in second_group:
+        print n,
+
+    print "\n"
+
+    input_file.close() # close the file
 
 if __name__ == "__main__":
     algorithms_final_project()
--- BasePage.py
+++ BasePage.py
@@ -3,12 +3,14 @@ import datetime
 import AlgorithmsFinalProject
 
 
+
 if '__main__' == __name__:
     number_of_nodes = 1000
     connectedness_of_graph = 100
-    name_of_test_file = "input-1000nodes-fullyconnected.txt"
+    name_of_test_file = "input.txt"
 
-    AlgorithmsFinalProject.graph_generator(number_of_nodes, connectedness_of_graph, name_of_test_file)
+    #AlgorithmsFinalProject.graph_generator(number_of_nodes, connectedness_of_graph, name_of_test_file)
     #AlgorithmsFinalProject.algorithms_final_project()
-    AlgorithmsFinalProject.adams_solution()
+
+    AlgorithmsFinalProject.adams_solution(name_of_test_file)
 
