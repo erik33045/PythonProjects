e7caad5274402788ba187d01c4bd6620b2f0ecdc
vtran21 <viktran@mail.usf.edu>
2014-04-19 01:12:14 -0400

    new input2.txt file added with disconnected graphs. Breaks Adam's solution!

--- BasePage.py
+++ BasePage.py
@@ -7,11 +7,11 @@ import AlgorithmsFinalProject
 if '__main__' == __name__:
     number_of_nodes = 1000
     connectedness_of_graph = 100
-    name_of_test_file = "input.txt"
+    name_of_test_file = "input2.txt"
 
     #AlgorithmsFinalProject.graph_generator(number_of_nodes, connectedness_of_graph, name_of_test_file)
     #AlgorithmsFinalProject.algorithms_final_project()
 
     #AlgorithmsFinalProject.adams_solution(name_of_test_file)
-    AlgorithmsFinalProject.random_cut(name_of_test_file, 10)
+    AlgorithmsFinalProject.random_cut(name_of_test_file, 20)
 
