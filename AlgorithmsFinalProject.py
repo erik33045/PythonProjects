fb4e7d12b2d4becb4dac6a6de8ed199842daaba0
erik <erik33045@gmail.com>
2014-04-19 13:36:17 -0400

    -Removed all warnings given by Pycharm -Made each function print output results to a file -Cleaned up some comments -Renamed adams_solution to rank_and_prune (any better names?) -Cleaned up the main function so that now it will call both solutions and pass the same file and output to two different files

--- AlgorithmsFinalProject.py
+++ AlgorithmsFinalProject.py
@@ -1,39 +1,22 @@
 import networkx as nx
-import random
-#import matplotlib.pyplot as plt
 import datetime
 import random
-import copy
 
 
-#randomization is a good factor to emply
-#Loop and do the problems multiple times, then select the best possible solution
 def algorithms_final_project():
-    import matplotlib.pyplot as plt
-    #Open the files
-    input_file = open("input.txt", 'r')
-    output_file = open("Hendrickson.txt", 'wb')
+    input_file_name = "input.txt"
 
-    #Read the number of expected datasets and place the file header in the correct place
-    line = input_file.readline().split()
-    number_of_nodes, number_of_edges = int(line[0]), int(line[1])
+    input_file = open(input_file_name, 'r')
+    output_file = open("output-rank.txt", 'wb')
+    rank_and_prune(input_file, output_file)
+    input_file.close()
+    output_file.close()
 
-    node_dictionary = {}
-    edge_list = []
-
-    for node in range(number_of_nodes):
-        node_dictionary[node+1] = str(node+1)
-
-    for edge in range(number_of_edges):
-        line = input_file.readline().split()
-        edge_tuple = (int(line[0]), int(line[1]))
-        edge_list.append(edge_tuple)
-
-    g = nx.Graph()
-    g.add_nodes_from(list(node_dictionary))
-    g.add_edges_from(edge_list)
-    nx.draw(g)
-    plt.savefig("output", dpi=90)
+    input_file = open(input_file_name, 'r')
+    output_file = open("output-random.txt", 'wb')
+    random_cut(input_file, output_file, 20)
+    input_file.close()
+    output_file.close()
 
 
 def graph_generator(number_of_nodes, connectedness_of_graph, name_of_test_file):
@@ -58,18 +41,20 @@ def graph_generator(number_of_nodes, connectedness_of_graph, name_of_test_file):
             output_file.write(str(node) + " " + str(edge) + "\n")
 
 
-def adams_solution(input_filename):
+def rank_and_prune(input_file, output_file):
     start_time = datetime.datetime.now()
-    g = nx.Graph() # create the graph for edge/node storage
-    input_file = open(input_filename, "r")
-    vals = input_file.readline().split() # grab the first two values nodes/edges
+    # create the graph for edge/node storage
+    g = nx.Graph()
+    # grab the first two values nodes/edges
+    vals = input_file.readline().split()
 
-    count = 0 # check the count of the current grouping
-    max_count = 0 # set the cap for the max count encountered
-    neighbors = [] # a list of the nodes with their number of neighbors
+    # check the count of the current grouping and set the cap for the max count encountered
+    count = max_count = 0
+    # a list of the nodes with their number of neighbors
+    neighbors = []
     first_group = []
-    second_group = [] # a second list used to form a second_group
-
+    # a second list used to form a second_group
+    second_group = []
 
     # read in each line and add the corresponding edge to the graph, g
     for i in range(1, int(vals[1])+1):
@@ -81,59 +66,61 @@ def adams_solution(input_filename):
 
     # place all the nodes and their neighbors into the neighbors list
     for i in range(1, len(g.nodes())+1):
-        neighbors.append([i,g[i].keys()])
+        neighbors.append([i, g[i].keys()])
 
     # the number of nodes to check (default to all of them)
     node_num = len(g.nodes())
 
     # check nodes until you reach the cap of num_nodes
     for i in range(0, node_num):
-        second_group.append(neighbors[i][0]) # place the next node into the second group
-        last_node = second_group[-1] # create a reference to the last node put into the second group
+        # place the next node into the second group
+        second_group.append(neighbors[i][0])
+        # create a reference to the last node put into the second group
+        last_node = second_group[-1]
         # set crossing_edges to the number of neighbors the most recently added node has
         # and remove any connections to nodes already in the group
         crossing_edges = len(g.neighbors(last_node)) - (len(set(g.neighbors(last_node)).intersection(second_group))) * 2
-        count += crossing_edges # append these crossing_edges to the count of crossing edges
-        if count > max_count: # if the current configuration is better than the previous best...
-            max_count = count # set this new max value
-        elif count < max_count: # if this new configuration isn't better than the best...
-            del second_group[-1] # remove the most recently added node and keep going
-            first_group.append(neighbors[i][0])#append to first_group
+        # append these crossing_edges to the count of crossing edges
+        count += crossing_edges
+        # if the current configuration is better than the previous best...
+        if count > max_count:
+            # set this new max value
+            max_count = count
+        # if this new configuration isn't better than the best...
+        elif count < max_count:
+            # remove the most recently added node and keep going
+            del second_group[-1]
+            #append to first_group
+            first_group.append(neighbors[i][0])
 
     elapsed_time = datetime.datetime.now() - start_time
 
-    print elapsed_time.microseconds*10^3
-    print max_count
+    output_file.write(str(float(elapsed_time.microseconds) / float(1000)) + "\n")
+    output_file.write(str(max_count) + "\n")
 
-    for n in first_group:
-        print n,
+    #write all entries in the first group seperated by spaces excluding the last entry
+    [output_file.write(str(n) + " ") for n in first_group[0:-1]]
+    #write the last entry with a new line
+    output_file.write(str(first_group[-1]) + '\n')
 
-    print
-
-    for n in second_group:
-        print n,
-
-    print "\n"
-
-    input_file.close() # close the file
+    #write all entries in the second group seperated by spaces excluding the last entry
+    [output_file.write(str(n) + " ") for n in second_group[0:-1]]
+    #write the last entry with a new line
+    output_file.write(str(second_group[-1]) + '\n')
 
 
-def random_cut(input_filename, run_time):
+def random_cut(input_file, output_file, seconds_to_run):
 
-    start_time = datetime.datetime.now();
+    start_time = datetime.datetime.now()
     elapsed_time_seconds = 0
 
     g = nx.Graph()
-
-    input_file = open(input_filename, 'r')
-    output_file = open("Tran.txt", 'wb')
 
     line = input_file.readline().split()
     number_of_nodes, number_of_edges = int(line[0]), int(line[1])
 
     node_dictionary = {}
     edge_list = []
-    node_list = []
 
     final_cross_count = 0
     final_first_group = []
@@ -147,36 +134,34 @@ def random_cut(input_filename, run_time):
         edge_tuple = (int(line[0]), int(line[1]))
         edge_list.append(edge_tuple)
 
-    g.add_nodes_from(list(node_dictionary), group = 1)
+    g.add_nodes_from(list(node_dictionary), group=1)
     g.add_edges_from(edge_list)
 
     node_list = g.nodes()
 
-
-    while(elapsed_time_seconds < run_time):
+    while elapsed_time_seconds < seconds_to_run:
         #set group attribute of random number of nodes to group 2
-        num_choose = random.randint(0,number_of_nodes)
+        num_choose = random.randint(0, number_of_nodes)
         for i in range(num_choose):
-            change_node = random.randint(1,number_of_nodes)
-            g.node[change_node]["group"]= 2
+            change_node = random.randint(1, number_of_nodes)
+            g.node[change_node]["group"] = 2
 
         first_group = []
         second_group = []
         crossing_edges = 0
 
         for x in node_list:
-            if(g.node[x]["group"] == 1):
+            if g.node[x]["group"] == 1:
                 first_group.append(x)
             else:
                 second_group.append(x)
 
         for x in first_group:
             for neighbor in g[x].keys():
-                if(g.node[neighbor]["group"] == 2 ):
+                if g.node[neighbor]["group"] == 2:
                     crossing_edges += 1
 
-
-        if (crossing_edges > final_cross_count):
+        if crossing_edges > final_cross_count:
             final_cross_count = crossing_edges
             final_first_group = first_group[:]
             final_second_group = second_group[:]
@@ -185,28 +170,23 @@ def random_cut(input_filename, run_time):
         for x in node_list:
             g.node[x]["group"] = 1
 
-        elapsed_time = datetime.datetime.now() - start_time
-        elapsed_time_seconds = elapsed_time.seconds
-
+        elapsed_time_seconds = (datetime.datetime.now() - start_time).seconds
 
     elapsed_time = datetime.datetime.now() - start_time
-    print elapsed_time.microseconds*10^3
 
-    print final_cross_count
+    output_file.write(str(float(elapsed_time.microseconds) / float(1000)) + '\n')
+    output_file.write(str(final_cross_count) + '\n')
 
-    for x in final_first_group:
-        print x,
+    #write all entries in the first group seperated by spaces excluding the last entry
+    [output_file.write(str(n) + " ") for n in final_first_group[0:-1]]
+    #write the last entry with a new line
+    output_file.write(str(final_first_group[-1]) + '\n')
 
-    print
-
-    for x in final_second_group:
-        print x,
-
-
-
+    #write all entries in the second group seperated by spaces excluding the last entry
+    [output_file.write(str(n) + " ") for n in final_second_group[0:-1]]
+    #write the last entry with a new line
+    output_file.write(str(final_second_group[-1]) + '\n')
 
 
 if __name__ == "__main__":
     algorithms_final_project()
-
-
--- BasePage.py
+++ BasePage.py
@@ -1,17 +1,4 @@
-# noinspection PyUnresolvedReferences
-import datetime
 import AlgorithmsFinalProject
 
-
-
 if '__main__' == __name__:
-    number_of_nodes = 1000
-    connectedness_of_graph = 100
-    name_of_test_file = "input2.txt"
-
-    #AlgorithmsFinalProject.graph_generator(number_of_nodes, connectedness_of_graph, name_of_test_file)
-    #AlgorithmsFinalProject.algorithms_final_project()
-
-    #AlgorithmsFinalProject.adams_solution(name_of_test_file)
-    AlgorithmsFinalProject.random_cut(name_of_test_file, 20)
-
+    AlgorithmsFinalProject.algorithms_final_project()
