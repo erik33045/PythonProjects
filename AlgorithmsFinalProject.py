e4c389d3e59af67de5d42f65740908baf9b87795
erik <erik33045@gmail.com>
2014-04-24 10:09:00 -0400

    Final updates to the project. Removing warnings and making the code more clean.

--- AlgorithmsFinalProject.py
+++ AlgorithmsFinalProject.py
@@ -2,7 +2,6 @@ import os
 import networkx as nx
 import datetime
 import random
-import shutil
 
 
 #Main function
@@ -12,40 +11,23 @@ def algorithms_final_project():
     input_file_name = "input.txt"
     input_file = open(input_file_name, 'r')
 
-    # parse input into graph
-    G = nx.Graph()
-
     # get number of nodes and edges
     line = input_file.readline().split()
     number_of_nodes, number_of_edges = int(line[0]), int(line[1])
 
-    # add each node to graph
-    # Time Complexity: O(m)*time_to_add_node
-    for node in range(number_of_nodes):
-        G.add_node(node+1, group=1)
-
-    # read in each line and add the corresponding edge to the graph, g
-    # Time Complexity: O(m)*time_to_add_edge
-    for edge in range(number_of_edges):
-        line = input_file.readline().split()
-        G.add_edge(int(line[0]), int(line[1]))
+    #Create the graph of nodes and edges
+    g = generate_graph(input_file, number_of_edges, number_of_nodes)
 
     input_file.close()
 
-
     #Perform the linear maximization algorithm
     output_file = open("output-linear.txt", 'wb')
-    linear_found_edges = linear_maximization(output_file, G, number_of_nodes)
+    linear_found_edges = linear_maximization(output_file, g, number_of_nodes)
     output_file.close()
 
     #Perform the random cut algorithm
     output_file = open("output-random.txt", 'wb')
-    random_found_edges = random_cut(output_file, G, number_of_nodes, 10)
-    output_file.close()
-
-
-    output_file = open("output-check.txt", 'wb')
-    check_output(output_file, G, number_of_nodes, "output-linear.txt")
+    random_found_edges = random_cut(output_file, g, number_of_nodes, 10)
     output_file.close()
 
     elapsed_time = datetime.datetime.now() - start_time
@@ -67,14 +49,25 @@ def algorithms_final_project():
     input_file.close()
     output_file.close()
 
-
-
-
     #Remove temporary output files
-    #os.remove("output-random.txt")
-    #os.remove("output-linear.txt")
+    os.remove("output-random.txt")
+    os.remove("output-linear.txt")
 
 
+def generate_graph(input_file, number_of_edges, number_of_nodes):
+    # parse input into graph
+    g = nx.Graph()
+    # add each node to graph
+    # Time Complexity: O(m)*time_to_add_node
+    for node in range(number_of_nodes):
+        g.add_node(node + 1, group=1)
+
+    # read in each line and add the corresponding edge to the graph, g
+    # Time Complexity: O(m)*time_to_add_edge
+    for edge in range(number_of_edges):
+        line = input_file.readline().split()
+        g.add_edge(int(line[0]), int(line[1]))
+    return g
 
 
 #Test function that generates a number of nodes and edges based on user inputs
@@ -115,7 +108,6 @@ def total_time_in_milliseconds(elapsed_time):
 #This Algorithm will go through each node and attempt to add it to group a, and check if going from B to A increases
 #the number of crossing edges
 def linear_maximization(output_file, g, number_of_nodes):
-    start_time = datetime.datetime.now()
 
     #initialize current crossing edge count to 0
     max_count = 0
@@ -128,16 +120,13 @@ def linear_maximization(output_file, g, number_of_nodes):
     # a second list used to form a second_group
     second_group = []
 
-        
     # place all the nodes into list to be shuffled and initialize the group 1 list to contain all nodes
     # Time Complexity: O(n)
     for i in range(1, number_of_nodes+1):
         nodes.append(i)
         first_group.append(i)
 
-
     random.shuffle(nodes)
-
 
     # for all nodes
     # Time Complexity: O(mn)
@@ -166,10 +155,6 @@ def linear_maximization(output_file, g, number_of_nodes):
             first_group.remove(current_node)
         # if this new configuration isn't better than the best...
 
-
-    elapsed_time = datetime.datetime.now() - start_time
-
-    #output_file.write(str(total_time_in_milliseconds(elapsed_time)) + "\n")
     output_file.write(str(max_count) + "\n")
 
     #write all entries in the first group seperated by spaces excluding the last entry
@@ -243,9 +228,6 @@ def random_cut(output_file, g, number_of_nodes, seconds_to_run):
 
         elapsed_time_seconds = (datetime.datetime.now() - start_time).seconds
 
-    elapsed_time = datetime.datetime.now() - start_time
-
-    #output_file.write(str(total_time_in_milliseconds(elapsed_time)) + '\n')
     output_file.write(str(final_cross_count) + '\n')
 
     #write all entries in the first group seperated by spaces excluding the last entry
@@ -260,23 +242,25 @@ def random_cut(output_file, g, number_of_nodes, seconds_to_run):
 
     return final_cross_count
 
-def check_output(output_file, g, number_of_nodes,  solution_filename):
 
+def check_output(output_file, g, solution_filename):
+    """ Function that checks if a given number of crossing edges is correct
+     for an input graph and an output file"""
     node_list = g.nodes()
 
     #get output groups
     solution_file = open(solution_filename, 'r')
 
-    reported_crossing_edges = int(solution_file.readline()) #get reported edges
+    #get reported edges
+    reported_crossing_edges = int(solution_file.readline())
 
     # create node group lists
     group_1 = map(int, solution_file.readline().split())
     group_2 = map(int, solution_file.readline().split())
 
-
     #reset group attributes
     for node in node_list:
-        g.node[node]["group"]=-1
+        g.node[node]["group"] = -1
 
     # set group attributes depending on the group
     for g1_vertex in group_1:
@@ -287,7 +271,7 @@ def check_output(output_file, g, number_of_nodes,  solution_filename):
 
     # check to make sure all nodes are properly labeled
     for n in node_list:
-        if g.node[n]["group"]==-1:
+        if g.node[n]["group"] == -1:
             output_file.write("All nodes not included in groups! Bad solution!\n")
             return
 
@@ -302,7 +286,7 @@ def check_output(output_file, g, number_of_nodes,  solution_filename):
     if crossing_edges == reported_crossing_edges:
         output_file.write("This is a correct solution. Hooray!")
     elif crossing_edges > reported_crossing_edges:
-        output_file.write("Not all crossing edges reported. It should be "+ str(crossing_edges) +"\n")
+        output_file.write("Not all crossing edges reported. It should be " + str(crossing_edges) + "\n")
     else:
         output_file.write("More crossing edges reported than there exist. EPIC FAILURE\n")
 
--- BasePage.py
+++ BasePage.py
@@ -1,13 +1,4 @@
 import AlgorithmsFinalProject
 
 if '__main__' == __name__:
-    '''file = open('input_1500nodes_fully_connected.txt', 'wb')
-    num_v = 1500
-    num_edges = num_v*(num_v-1)
-    file.write(str(num_v) + " " + str(num_edges) + "\n")
-    for vertex in range(num_v):
-        for neighbor in range(num_v):
-            if neighbor != vertex:
-                file.write(str(vertex+1) + " " + str(neighbor + 1) + "\n")
-    file.close()'''
     AlgorithmsFinalProject.algorithms_final_project()
